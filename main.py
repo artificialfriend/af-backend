7ac89bbf0f5979fbaf072288e773206b240e0cd1
velvetentropy <ashutoshnarang80@gmail.com>
2023-01-02 00:43:05 +0530

    add empty routes

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# GET Requests
+
 
 @app.get("/")
 def root():
@@ -31,3 +33,26 @@ def root():
 def chat(prompt: str):
     gpt3 = Gpt3()
     return gpt3.generate_essay(prompt)
+
+
+@app.get("/user")
+def get_user():
+    return "not implemented yet"
+
+
+@app.get("/event")
+def get_event():
+    return "not implemented yet"
+
+# PUT Requests
+
+
+@app.put("/user")
+def create_user():
+    return "not implemented yet"
+
+
+@app.put("/event")
+def create_event():
+    return "not implemented yet"
+
