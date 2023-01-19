d6158bd984a873d2de7d13bb28b9d6ac7f319e6a
velvetentropy <ashutoshnarang80@gmail.com>
2023-01-20 02:23:48 +0530

    patch gpt3 test

--- tests/test_gpt3.py
+++ tests/test_gpt3.py
@@ -15,14 +15,16 @@ def mock_response():
         "id": "cmpl-6UJOHaJWAuYx5Pbg8kfFjkRJodi4p",
         "model": "text-davinci-003",
         "object": "text_completion",
-        "usage": {"completion_tokens": 11, "prompt_tokens": 1, "total_tokens": 12},
+        "usage": {
+            "completion_tokens": 11,
+            "prompt_tokens": 1,
+            "total_tokens": 12,
+        },
     }
 
 
 def test_generate_essay(mocker):
-    mocker.patch(
-        "pretrained_transformer.gpt3.send_request", return_value=mock_response()
-    )
+    mocker.patch("pretrained_transformer.gpt3.send_request", return_value=mock_response())
     response = generate_essay("hello")
     print(response)
     assert response == "\n\nHi there! How can I help you?"
