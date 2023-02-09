0989fbab9963f78f40ab87f6fe0dd8510017e305
conscioustahoe <ashutoshnarang80@gmail.com>
2023-02-10 00:27:43 +0530

    make email non optional

--- src/schema/user.py
+++ src/schema/user.py
@@ -1,13 +1,11 @@
 from datetime import datetime
-from typing import Optional
-
 from pydantic import BaseModel
 
 
 class User(BaseModel):
     user_id: str
     af_id: str
-    email: Optional[str] = None
+    email: str
     first_name: str
     last_name: str
     birth_date: datetime
