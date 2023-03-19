59d4fd80f102f1c866cfc920c302899c41492de7
coscioustahoe <ashutoshnarang80@gmail.com>
2023-03-19 21:11:45 +0530

    add model field

--- src/schema/chat.py
+++ src/schema/chat.py
@@ -8,6 +8,7 @@ class Chat(BaseModel):
     text: str
     is_prompt: Optional[bool] = True
     behaviour: Optional[str] = "You are a helpful assistant."
+    model: Optional[str] = "text-davinci-003"
 
     class Config:
         orm_mode = True
